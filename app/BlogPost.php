8a6995f6cabc78d7741fb6cd4840103dfb8569e1
vagrant <vagrant@homestead>
2019-12-09 10:57:16 +0000

    [add]Comment model && migration

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -9,4 +9,10 @@ class BlogPost extends Model
 //    protected $table = 'blog_posts';
 
       protected $fillable = ['title', 'content'];
+
+      public function comments()
+      {
+          return $this->hasMany('App\Comment');
+      }
+
 }
---
+++ app/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function blogPost()
+    {
+//        return $this->belongsTo('App\BlogPost', 'post_id', 'blog_post_id');
+        return $this->belongsTo('App\BlogPost');
+
+    }
+}
